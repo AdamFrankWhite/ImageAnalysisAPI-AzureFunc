523adcab03986fe6628ca0246b1467e2865ddd7f
Adam White <adamfrankwhite@gmail.com>
2024-01-03 20:25:50 +0000

    added image model class

---
+++ Models/Image.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAnalysisAPI.models { 
+
+    internal class Image
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Filename")]
+        public string Filename { get; set; } = null!;
+        [BsonElement("ImageUrl")]
+        public string ImageUrl { get; set; } = null!;
+        [BsonElement("ExplicitCategories")]
+        public List<ExplicitCategory> ExplicitCategories { get; set; } = null!;
+        [BsonElement("ExplicitParentCategories")]
+        public List<ExplicitCategory> ExplicitParentCategories { get; set; } = null!;
+    }
+}
