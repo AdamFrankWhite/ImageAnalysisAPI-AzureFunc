ca8211de68a12772b88f837433be440a191111bc
Adam White <adamfrankwhite@gmail.com>
2024-01-03 20:26:22 +0000

    added databaseSettings model

---
+++ models/ImageModerationDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAnalysisAPI.models
+{
+    internal class ImageModerationDatabaseSettings
+    {
+       
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string ImagesCollectionName { get; set; } = null!;
+    }
+
+}
+
