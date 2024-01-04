93efef9a10ed66b5707fee38a1397daf320a373f
Adam White <adamfrankwhite@gmail.com>
2024-01-04 22:04:36 +0000

    added model

---
+++ models/DetectionResult.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Rekognition.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageAnalysisAPI.models
+{
+    public class DetectionResult
+    {
+        public DetectionResult() { }
+        public List<Label> Labels { get; set; }
+        public List<ModerationLabel> ExplicitContent { get; set; }
+    }
+
+}
